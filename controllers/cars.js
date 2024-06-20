720b7cdd953df04337f2a600be865a588d140a2b
Samagamalll <sama2203286@miuegypt.edu.eg>
2024-06-21 01:44:43 +0300

    updateCar Function

--- controllers/cars.js
+++ controllers/cars.js
@@ -44,6 +44,41 @@ const GetCars = async (req, res) => {
     res.render(`cars/${req.params.name}`, { cars: ((car) ? car : []), user: (req.session.user === undefined ? "" : req.session.user) });
       
 };
+const UpdateCar = async (req, res) => {
+    try {
+        console.log(req.body.methodtype)
+        if (req.body.methodtype === "update") {
+            console.log(req.body)
+            const temp = await Cars.findOneAndUpdate({ Name: req.body.carName }, {
+                Model: req.body.carModel,
+                Price: req.body.carPrice,
+                Color: req.body.carColor,
+                Engine: req.body.engineCapacity,
+                Power: req.body.power,
+                Transmission: req.body.transmission,
+                Fuel: req.body.fuel,
+                Year: req.body.year,
+                Image: req.body.imgurl
+            });
+            if (!temp) {
+                return res.status(404).send('Car not found');
+            }
+            
+        }
+        else if (req.body.methodtype === "delete") {
+            //console.log("DELETING SUCCESS")
+
+            const temp = await Cars.findOneAndDelete({ Name: req.body.carName })
+            if (!temp) {
+                return res.status(404).send('Car not found');
+            }
+        }
+    }
+    catch (error) {
+        res.status(500).send('Error');
+    }
+    res.redirect('/Cars')
+};
 
 module.exports = {
     AddCar,
